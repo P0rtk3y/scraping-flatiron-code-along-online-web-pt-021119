48568fe3b337e73f38785e299b65811653daead0
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-28 06:26:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,12 +14,15 @@ class Scraper
     #   course.schedule = post.css(".date").text 
     #   course.description = post.css("p").text 
     # end 
-   
+  end 
+  
+  def get_courses 
+    self.get_page.css(".post")
   end 
   
   
   def print_courses
-  self.make_courses
+    self.make_courses
     Course.all.each do |course|
       if course.title
         puts "Title: #{course.title}"
