6105d6884e0cdbd20d44d05df07a5f88b47a797a
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-28 06:25:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,8 +18,8 @@ class Scraper
   end 
   
   
-    def print_courses
-    self.make_courses
+  def print_courses
+  self.make_courses
     Course.all.each do |course|
       if course.title
         puts "Title: #{course.title}"
